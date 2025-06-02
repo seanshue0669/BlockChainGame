a2096e032550f8ebb65c681a0ac9b622dc1da04e
seanshue <seanshue0423@gmail.com>
2025-06-02 19:21:50 +0800

    feat
    Binding buutton event

--- Assets/Resource/UIToolkit/MainMenu/MainUIController.cs
+++ Assets/Resource/UIToolkit/MainMenu/MainUIController.cs
@@ -3,6 +3,10 @@ using UnityEngine.UIElements;
 public class MainUIController : MonoBehaviour
 {
     private VisualElement _title;
+    private Button _startButton;
+    private Button _settingsButton;
+    private Button _lotteryButton;
+    private Button _marketButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,13 +17,12 @@ public class MainUIController : MonoBehaviour
         {
             _title = root.Q<VisualElement>("GameTitle");
         }
-
+        bindUIEvent(root);
 
         // Start The looping animaton
         Invoke("LoopAnimation", 0.1f);
+
     }
-
-
 
     private void LoopAnimation()
     {
@@ -29,4 +32,39 @@ public class MainUIController : MonoBehaviour
             evt => _title.ToggleInClassList("titleScale")
         );
     }
+
+    private void bindUIEvent(VisualElement root)
+    {
+        _startButton = root.Q<Button>("StartButton");
+        _settingsButton = root.Q<Button>("SettingButton");
+        _lotteryButton = root.Q<Button>("LotteryButton");
+        _marketButton = root.Q<Button>("MarketButton");
+
+        _startButton?.RegisterCallback<ClickEvent>(evt => OnStartClicked());
+        _settingsButton?.RegisterCallback<ClickEvent>(evt => OnSettingsClicked());
+        _lotteryButton?.RegisterCallback<ClickEvent>(evt => OnLotteryClicked());
+        _marketButton?.RegisterCallback<ClickEvent>(evt => OnMarketClicked());
+    }
+    #region CB_Function
+    private void OnStartClicked()
+    {
+        Debug.Log("Start button clicked");
+    }
+
+    private void OnSettingsClicked()
+    {
+        Debug.Log("Settings button clicked");
+    }
+
+    private void OnLotteryClicked()
+    {
+        Debug.Log("Lottery button clicked");
+    }
+
+    private void OnMarketClicked()
+    {
+        Debug.Log("Market button clicked");
+    }
+
+    #endregion
 }
