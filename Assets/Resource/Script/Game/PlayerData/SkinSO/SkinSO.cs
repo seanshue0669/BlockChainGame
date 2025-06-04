02bbca139d4efae973f181817575a6c1cba73006
seanshue <seanshue0423@gmail.com>
2025-06-05 03:26:03 +0800

    feat
    add skn system

---
+++ Assets/Resource/Script/Game/PlayerData/SkinSO/SkinSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "SkinSO", menuName = "ScriptableObjects/SkinData")]
+public class SkinSO : ScriptableObject
+{
+    public int rarity;
+    public float wear;
+
+}
